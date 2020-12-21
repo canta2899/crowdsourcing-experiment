09113d00778c142834efbf5817721fa75879de7a
canta2899 <56071114+canta2899@users.noreply.github.com>
2020-12-21 15:47:09 +0100

    First commit with """"framework"""" working

---
+++ data/build/document.ts
@@ -0,0 +1,45 @@
+/*
+ * This class provides a representation of a single document stored in single hit stored in the Amazon S3 bucket.
+ * The attribute <index> is additional and should not be touched and passed in the constructor.
+ * Each field of such Document must be mapped to an attribute of this class and set up in the constructor as it is shown.
+ * Take care also in providing an implementation of the function; it is used to define if a document represents the high and low gold question.
+ */
+export class Document {
+
+  /* DO NOT REMOVE THIS ATTRIBUTE */
+  index: number;
+
+  name: string;
+  statement: string;
+  claimant: string;
+  date: string;
+  originatedFrom: string;
+  value: string;
+
+  constructor(
+    index: number,
+    data: JSON
+  ) {
+    /* DO NOT REMOVE THIS LINE */
+    this.index =          index;
+
+    this.name =           data["name"];
+    this.statement =      data["statement"];
+    this.claimant =       data["claimant"];
+    this.date =           data["date"];
+    this.value = 	  data["value"];
+    this.originatedFrom = data["originated-from"];
+  }
+
+  /*
+   * This function determines if the current document is a gold question.
+   * Possible values for the parameter: HIGH or LOW. (CAPS LOCK MANDATORY).
+   * In this case, for example, if the id_par field is HIGH (LOW) then
+   * the document is the HIGH (LOW) gold question
+   */
+  public getGoldQuestionIndex(kind: string) {
+    if (this.name == kind) return this.index;
+    return null
+  }
+
+}
---
+++ data/build/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+
+	production: false, 
+	configuration_local: true, 
+
+	taskName: 'Sample',
+	batchName: 'Batch-1',
+
+	region: 'eu-central-1',
+	bucket: 'socialcomputing2-tasks', 
+	aws_id_key: 'AKIAR2LXIOQECNFHOOUY',
+	aws_secret_key: 'zclCKDxhbfB1f4VejIuB6xlNHiE2aX3AryVcDGao',
+
+	bing_api_key: 'your_bing_key'
+
+}
