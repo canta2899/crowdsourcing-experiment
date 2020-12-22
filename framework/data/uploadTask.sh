9c90bd173bd2f809d11c64a6a67fda2350990207
bomb697 <50354770+bomb697@users.noreply.github.com>
2020-12-22 15:37:42 +0100

    Updated dimensions.json and added a new upload script

---
+++ framework/data/uploadTask.sh
@@ -0,0 +1,12 @@
+echo "Deploying task ${1}/${2}";
+aws s3api put-object     --bucket "${3}" --key "admin.json"                                   --body "tasks/admin.json"                                    --content-type application/json       ;
+echo "Uploading configuration to folder: ${3}/Task/";
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/dimensions.json"               --body "tasks/${1}/${2}/task/dimensions.json"                --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/hits.json"                     --body "tasks/${1}/${2}/task/hits.json"                      --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/instructions_main.json"        --body "tasks/${1}/${2}/task/instructions_main.json"         --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/instructions_dimensions.json"  --body "tasks/${1}/${2}/task/instructions_dimensions.json"   --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/questionnaires.json"           --body "tasks/${1}/${2}/task/questionnaires.json"            --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/search_engine.json"            --body "tasks/${1}/${2}/task/search_engine.json"             --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/task.json"                     --body "tasks/${1}/${2}/task/task.json"                      --content-type application/json       ;
+aws s3api put-object     --bucket "${3}" --key "${1}/${2}/Task/workers.json"                  --body "tasks/${1}/${2}/task/workers.json"                   --content-type application/json       ;
+echo "Upload completed";
